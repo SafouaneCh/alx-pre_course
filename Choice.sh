ad670ec357e961f475c32d6349192c80ab08f801
BernichiFZ <bernichifatimazahra.com>
2024-05-27 21:05:06 -0400

    FZ update

--- Choice.sh
+++ Choice.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+
 Show_Menu() {
     echo "To-Do List Menu:
+          your choices are: 
         1. View: View the To-Do List that you wish to see
-        2.1.Add1 : Add a Task to an existing list
-         .2.Add2 : Add a new list and new Tasks
+        2.1. Add1 : Add a Task to an existing list 
+         .2. Add2 : Add a new list and new Tasks
         3. Remove: Remove Task
-        4. Show Task: Show a specific Task
-        5. Edit: Edit a specific Task
+        4. Show-Task: Show a specific Task
+        5. Edit : Edit a specific Task
         6. Filter: Filter by status
         7. Priority: Prioritize Tasks
-        8. Allarm: Activate the alarm option
-        9. Press 0 : Exit"
+        9. Alarm: Activate the alarm option
+        10. Press 0 : Exit"
 
     read -p "Your choice: " choice
     Handle_Choice "$choice"
@@ -19,52 +21,59 @@ Show_Menu() {
 
 Handle_Choice() {
     case $1 in
-        1) Show_List ;;
-        2) Add_Task ;;
-        3) Add_List ;;
+        View) Show_List ;;
+        Add1) Add_Task ;;
+        Add2) Add_List ;;
+	Show-Task) Show_Task ;;
         0) exit ;;
         *) echo "Invalid choice";;
     esac
-
 }
 
 Show_List() {
+    cd "to_do_list "
     echo "Enter the name of the list you wish to view: "
     read name 
 
-    if [ -d "/home/kali/TO_DO_LIST/$name" ]; then
-        ls "/home/kali/TO_DO_LIST/$name"
+    if [ -d "$name" ]; then
+        ls "$name"
     else 
         echo "There's no such list in your machine"
     fi
 }
 
 Add_Task() {
-    n=$(ls -l /home/kali/TO_DO_LIST | wc -l)
-    let n=n-1
-    mkdir "/home/kali/TO_DO_LIST/Task$n" || exit
-    touch "/home/kali/TO_DO_LIST/Task$n/Task$n.txt" || exit
+    echo "_ In which list does your Task exist : "
+    read name
+
+    n=$(ls $name| wc -l)
+    let n=$n+1
+    mkdir "$name/Task$n" || exit
+    cd "$name/Task$n"
+    touch "Task$n.txt" || exit
+
+    
 
     echo "_ What's your task title: "
     read title
-    echo "- Title: $title" >> "/home/kali/TO_DO_LIST/Task$n/Task$n.txt"
+    echo "- Title: $title" >> Task$n.txt
 
     echo "_ What's the deadline of your task: "
     read deadline
-    echo "- Deadline: $deadline" >> "/home/kali/TO_DO_LIST/Task$n/Task$n.txt"
+    echo "- Deadline: $deadline" >> Task$n.txt
 
     date=$(date)
-    echo "- Added in: $date" >> "/home/kali/TO_DO_LIST/Task$n/Task$n.txt"
+    echo "- Added in: $date" >> Task$n.txt
 
-    echo "- Status: ongoing" >> "/home/kali/TO_DO_LIST/Task$n/Task$n.txt"
+    echo "- Status: ongoing" >> Task$n.txt
 
     echo "_ Who works on this task: "
     read assignees
-    echo "- Assignees: $assignees" >> "/home/kali/TO_DO_LIST/Task$n/Task$n.txt"
+    echo "- Assignees: $assignees" >> Task$n.txt
 
     echo "_ What do you need to complete this task: "
     read attachments
-    echo "- Attachments: $attachments" >> "/home/kali/TO_DO_LIST/Task$n/Task$n.txt"
+    echo "- Attachments: $attachments" >> Task$n.txt
 
     echo "_ Do you wish to add any sub-tasks (y/n): "
     read answer
@@ -74,7 +83,7 @@ Add_Task() {
             echo "No sub-tasks added"
             ;;
         y|Y) 
-            ./Add_subTask.sh "Task$n"
+            Add_subTask "Task$n"
             echo "SubTask added successfully"
             ;;
         *) 
@@ -84,17 +93,73 @@ Add_Task() {
 }
 
 Add_List() {
-    n=$(ls -l /home/kali/TO_DO_LIST | wc -l)
-    let n=n-1
-    mkdir "/home/kali/TO_DO_LIST/List$n" || exit
+	n=$(ls  | wc -l)
+    let n=$n-1
+    mkdir "List$n" || exit
     echo "List$n created successfully"
 }
 
-# Keep showing the menu until user exits
-while true; do
-    Show_Menu
-    if [ "$choice" -eq 0 ]; then
-        exit
-    fi
-done
 
+
+Add_subTask() {
+    local list="$1"
+
+    k=$(ls "$list" | wc -l)
+
+    cd "$list"
+    touch "Sub_Task$k.txt"
+
+    read -p "What's your task title: " title
+    echo "- Title: $title" >> "Sub_Task$k.txt"
+
+    read -p "What's the deadline of your task: " deadline
+    echo "- Deadline: $deadline" >> "Sub_Task$k.txt"
+
+    date=$(date)
+    echo "- Added in: $date" >> "Sub_Task$k.txt"
+
+    read -p "Who works on this task: " assignees
+    echo "- Assignees: $assignees" >> "Sub_Task$k.txt"
+
+    read -p "What do you need to complete this task: " attachments
+    echo "- Attachments: $attachments" >> "Sub_Task$k.txt"
+
+    read -p "Do you wish to add other sub-tasks (y/n): " answer
+
+    case $answer in
+        n|N) echo "No other sub-tasks added" ;;
+        y|Y) Add_subTask "$list" ;;
+        *) echo "Incorrect Input" ;;
+    esac
+}
+
+Show_Task(){
+	
+	echo " In what list is your task : "
+	read list
+        echo "Which task exactly : "
+	read Task
+        for j in $(ls "$list/$Task" ); do
+        if [[ "$j" == Task* ]]; then
+            echo "The main task is : "
+            echo -e
+            cat "$list/$Task/$j"
+            echo -e
+        elif [[ "$j" == Sub* ]]; then
+            echo "The sub tasks are : "
+            echo -e
+            cat "$list/$Task/$j"
+            echo -e
+        fi
+    done
+}
+
+
+# Keep showing the menu until the user exits
+while true; do
+    
+    if [[ $choice == 0 ]]; then 
+	    exit 
+    fi
+    Show_Menu
+done
